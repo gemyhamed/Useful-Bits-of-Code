36335f370c8ca3a25292986e434a46d5d503f5b9
Mohamed Gamal Hamed <47461269+gemyhamed@users.noreply.github.com>
2019-03-21 01:56:22 +0200

    Add files via upload

---
+++ Code To Visualize Logistic Regression.py
@@ -0,0 +1,42 @@
+# Visualising the Test set results
+from matplotlib.colors import ListedColormap
+# create a copy of X_train and y_train
+# for code resusability
+X_set, y_set = x_train, y_train
+# generate coordinate matrics using meshgrid
+X1, X2 = np.meshgrid(np.arange(start=X_set[:, 0].min()-1, stop=X_set[:, 0].max()+1, step=0.01),
+ np.arange(start=X_set[:, 1].min()-1, stop=X_set[:, 1].max()+1, step=0.01))
+print(X1)
+print(X2)
+# now we need to plot the prediction for every coordinate from X1 and X2 therefore we need to first get the prediction from coordinate matrics
+# convert X1 coordinate matrix to a flattened array - putting all the elements in one column
+X1_ravel = X1.ravel()
+# convert X2 coordinate matrix to a flattened array - putting all the elements in one column
+X2_ravel = X2.ravel()
+# create an array having 2 rows by placing X1_ravel over X2_ravel
+X1X2_array = np.array([X1_ravel, X2_ravel])
+# Since predict function takes an array which has 2 columns therefore we need to generate Transpose of X1X2_array - columns are converted into rows
+X1X2_array_t = X1X2_array.T
+# predict result using the classifier
+X1X2_pred = cls.predict(X1X2_array_t)
+#result of prediction will be used to plot againt the coordinate matrics therefore we need to reshape the result to match the shape of coordinate matrics
+#generated array contains prediction for every coordinate value
+X1X2_pred_reshape = X1X2_pred.reshape(X1.shape)
+#plot the predictions against coordinate matrics using contourf (filled)
+result_plt = plt.contourf(X1, X2, X1X2_pred_reshape,
+ alpha=0.75,
+ cmap = ListedColormap(('red', 'green'))
+)
+#not mandatory
+#plt.xlim(X1.min(), X1.max())
+#plt.ylim(X2.min(), X2.max())
+#plot the actual points on the graph
+for i, j in enumerate(np.unique(y_set)):
+ plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+ c = ListedColormap(('red', 'green'))(i), label = j)
+#for housekeeping
+plt.title('Logistic Regression (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
